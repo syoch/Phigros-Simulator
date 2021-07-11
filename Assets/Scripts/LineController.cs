d25013d0cd19e0b75f8690a4741ef3523dc16cff
syoch <hacksyoch@gmail.com>
2021-07-11 17:47:19 +0900

    update: add notes attribute in LineController

--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class LineController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public GameObject[] notes;
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
 }
