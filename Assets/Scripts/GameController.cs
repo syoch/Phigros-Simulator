3664421f3c400028ae9a19b10cfc5ef8dc6b36b7
syoch <hacksyoch@gmail.com>
2021-07-11 10:44:50 +0900

    update: add bar detector

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+  public float BarTime;
   // Start is called before the first frame update
   void Start()
   {
-
+    BarTime = 60f / 200f;
   }
 
   // Update is called once per frame
   void Update()
   {
-
+    Debug.LogFormat("{0} {1}", Mathf.Round(Time.time / BarTime), Time.time % BarTime);
   }
 }
