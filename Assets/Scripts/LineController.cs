d99ea140911568f7a379f37d330760c19e53cf96
syoch <hacksyoch@gmail.com>
2021-07-11 17:51:24 +0900

    update: change type of notes

--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LineController : MonoBehaviour
 {
-  public GameObject[] notes;
+  public List<GameObject> notes;
   // Start is called before the first frame update
   void Start()
   {
