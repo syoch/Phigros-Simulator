4b9f357a8c3ebcd6b6a1f9c7b762200aed056c11
syoch <hacksyoch@gmail.com>
2021-07-11 13:44:21 +0900

    update: add some note classess

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[SerializeField]
+public class NoteBase
+{
+  public int[] timing;
+  public int line;
+  public string type;
+}
+[SerializeField]
+public class Note : NoteBase
+{
+  public double pos; // -120 ~ 120
+}
+
+[SerializeField]
+public class NewLine : NoteBase
+{
+  public double[] pos;
+  double rot;
+}
+[SerializeField]
+public class MoveLine : NoteBase
+{
+  public double[] pos;
+  public double rot;
+}
+
+[SerializeField]
+public class Speed : NoteBase
+{
+  public double rot;
+}
+[SerializeField]
+public class Chart
+{
+  public string name;
+  public int line_count;
+
+}
 
 public class GameController : MonoBehaviour
 {
