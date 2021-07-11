0a2551eb3a0501f177583846d7741fe7636586d5
syoch <hacksyoch@gmail.com>
2021-07-11 23:26:06 +0900

    update: impl motion

--- Assets/Scripts/NoteController.cs
+++ Assets/Scripts/NoteController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class NoteController : MonoBehaviour
 {
+  public float StartPos;
   // Start is called before the first frame update
   void Start()
   {
-
+    StartPos = transform.position.y;
   }
 
   // Update is called once per frame
   void Update()
   {
+    var pos = transform.position;
 
+    pos.y = StartPos
+          - (Time.time - GameController.StartTime)
+            / GameController.Instance.BarTime
+            * (float)GameController.Instance.a;
+    transform.position = pos;
   }
 }
