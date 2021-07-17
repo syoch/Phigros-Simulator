386edc3baa2c2f244459f421a1956d630368947f
syoch <hacksyoch@gmail.com>
2021-07-17 12:30:28 +0900

    update: add Rotate parse

--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -1,15 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public class LineRotate
+{
+  public double Start;
+  public double End;
+  public double Deg;
+}
 
 public class LineController : MonoBehaviour
 {
   private Line _line;
-  public Line line{
-    set{
+  public Line line
+  {
+    set
+    {
       _line = value;
     }
   }
+  public LineRotate[] rotates;
   // Start is called before the first frame update
   void Start()
   {
@@ -20,6 +30,23 @@ public class LineController : MonoBehaviour
   void Update()
   {
 
+  }
+  public IEnumerator Load()
+  {
+    rotates = new LineRotate[_line.rotates.Length];
+    LineRotate data;
+    int i = 0;
+    foreach (var rotate in _line.rotates)
+    {
+      data = new LineRotate();
+      data.Start = rotate.start[0] + rotate.start[1] / rotate.start[2];
+      data.End = rotate.end[0] + rotate.end[1] / rotate.end[2];
+      data.Deg = rotate.val;
+      rotates[i] = data;
+      yield return null;
+      i++;
+    }
+    Array.Sort(rotates, (a, b) => { return a.Start.CompareTo(b.Start); });
   }
   public void MakeNode(GameObject baseObject, double y, float x)
   {
